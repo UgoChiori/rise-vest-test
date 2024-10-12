32f38f07267deadcb29e7c1e7b64c1128be8d09e
checkr <ugochiori@gmail.com>
2024-10-12 11:24:09 +0100

    ...

--- src/components/AppPromotionSection.tsx
+++ src/components/AppPromotionSection.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const AppPromotionSection: React.FC = () => {
   return (
-    <section className="relative flex flex-col md:flex-row items-center h-auto h-[394px] py-16 md:py-24 bg-[#ECFEFE] text-center rounded-[30px] md:rounded-[50px] px-6 md:px-16 overflow-hidden">
+    <section className="relative flex flex-col md:flex-row items-center h-auto h-[394px] py-16 md:py-24 bg-[#ECFEFE] text-center rounded-[30px] md:rounded-[50px] px-6 md:px-16 overflow-hidden !important">
       <div className="md:w-1/2 md:text-left z-10">
         <h2 className="text-[#07969E] text-[14px] md:text-[16px] font-medium mb-2 md:mb-4">
           Download the Rise App
@@ -44,7 +44,7 @@ const AppPromotionSection: React.FC = () => {
         <img
           src="/images/heroimage.svg"
           alt="Phone"
-          className="w-[180px] md:w-[250px] object-contain relative md:absolute md:top-[-150px]"
+          className="w-[180px] md:w-[250px] object-contain relative md:absolute md:top-[-150px] !important"
         />
       </div>
     </section>
